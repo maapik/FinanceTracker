b4348b768bf3f080aefd8bc008ef4443d89be92b
maapik <nikitasedov270@gmail.com>
2026-05-20 15:19:46 +0300

    Fixed buttons and added gitignore

--- FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
+++ FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12abd1af40e3062dd2948469bc8cc0e4d3b108d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
